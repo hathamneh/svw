d17d59453f7cd3103ccde98ff486ba778dfd0d96
haitham <himoath@gmail.com>
2018-03-02 11:42:40 +0200

    redirect un registerd volunteer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,9 @@ class HomeController extends Controller
     public function index()
     {
         $user = auth()->user();
-        $volunteer = $user->volunteer;
+	$volunteer = $user->volunteer;
+	if(is_null($volunteer))
+		return redirect("/register/wizard");
         return view('home')->with('volunteer',$volunteer);
     }
 
