4c301d047cc488c3f03c3b9fdbea35c2ce60cb80
haitham <himoath@gmail.com>
2018-03-06 21:08:36 +0200

    logging

--- app/Http/Controllers/Api/WizardController.php
+++ app/Http/Controllers/Api/WizardController.php
@@ -7,16 +7,23 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\WizardController as Wizard;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class WizardController extends Controller
 {
     public function store(Request $request)
     {
         $user_data_raw = $request->get('data');
+
+        Log::debug(print_r($request->toArray(),true));
+        Log::debug($user_data_raw);
+
         if (is_array($user_data_raw))
             $user_data = json_decode(json_encode($user_data_raw));
         else
             $user_data = json_decode($user_data_raw);
+
+
 
         //return $user_data_raw;
         /**
