81653a116e21fa132a711a01cf03c48f23db186b
haitham <himoath@gmail.com>
2018-04-18 19:28:19 +0300

    blah blah

--- app/Http/Resources/OrganizationCollection.php
+++ app/Http/Resources/OrganizationCollection.php
@@ -17,6 +17,7 @@ class OrganizationCollection extends JsonResource
     {
         return [
             'id'              => $this->id,
+            'user_id'         => $this->user_id,
             'name'            => $this->name,
             'country'         => CountriesFacade::lookup()[$this->country] ?? $this->country,
             'city'            => is_null($this->city) ? "" : $this->city,
