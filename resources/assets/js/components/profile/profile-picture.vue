36786cf49bf733b1b97e53ce68e529b6ae75ddb3
haitham <himoath@gmail.com>
2018-04-19 16:40:27 +0300

    saving hahaha

--- resources/assets/js/components/profile/profile-picture.vue
+++ resources/assets/js/components/profile/profile-picture.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="profile-picture">
         <el-upload v-if="editable"
-                class="avatar-uploader"
-                action="/api/upload_image/profile"
-                :show-file-list="false"
-                :on-success="handleAvatarSuccess"
-                :before-upload="beforeAvatarUpload"
-                :http-request="uploadImage">
-            <img v-if="imageUrl" :src="imageUrl" class="avatar"
-                 v-loading="isUploading"
-                 element-loading-text="Loading..."
-                 element-loading-spinner="el-icon-loading"
-                 element-loading-background="rgba(0, 0, 0, 0.8)">
+                   class="avatar-uploader"
+                   action="/api/upload_image/profile"
+                   :show-file-list="false"
+                   :on-success="handleAvatarSuccess"
+                   :before-upload="beforeAvatarUpload"
+                   :http-request="uploadImage"
+                   v-loading="isUploading"
+                   element-loading-text="Loading..."
+                   element-loading-spinner="el-icon-loading"
+                   element-loading-background="rgba(0, 0, 0, 0.8)">
+            <img v-if="imageUrl" :src="imageUrl" class="avatar">
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             <div class="change-pp-btn"><i class="fa fa-camera fa-2x mb-1"></i> Change Picture</div>
         </el-upload>
@@ -49,11 +49,12 @@
                 axios
                     .post("/api/upload_image/profile", formData)
                     .then(response => {
-                        if(response.data.result && response.data.url)
+                        if (response.data.result && response.data.url)
                             this.imageUrl = response.data.url
 
                     })
-                    .catch(response => {});
+                    .catch(response => {
+                    });
             }
         }
     }
