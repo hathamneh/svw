fed2ce9f9bb384b6482bcff7803099270a197b63
haitham <himoath@gmail.com>
2018-04-20 20:05:55 +0300

    fix err

--- app/MyModel.php
+++ app/MyModel.php
@@ -14,23 +14,23 @@ use Illuminate\Database\Eloquent\Model;
 class MyModel extends Model
 {
 
-    public static function boot()
-    {
-        parent::boot();
-
-        static::saving(function($model)
-        {
-            if (count($model->forcedNullFields) > 0) {
-                foreach ($model->toArray() as $fieldName => $fieldValue) {
-                    if ( empty($fieldValue) && in_array($fieldName,$model->forcedNullFields)) {
-                        $model->attributes[$fieldName] = null;
-                    }
-                }
-            }
-
-            return true;
-        });
-
-    }
+//    public static function boot()
+//    {
+//        parent::boot();
+//
+//        static::saving(function($model)
+//        {
+//            if (count($model->forcedNullFields) > 0) {
+//                foreach ($model->toArray() as $fieldName => $fieldValue) {
+//                    if ( empty($fieldValue) && in_array($fieldName,$model->forcedNullFields)) {
+//                        $model->attributes[$fieldName] = null;
+//                    }
+//                }
+//            }
+//
+//            return true;
+//        });
+//
+//    }
 
 }
