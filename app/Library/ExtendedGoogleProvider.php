5381b1df52ff98a3f7f1311b9a278fe59cafacc1
haithamAth <himoath@gmail.com>
2018-05-03 15:55:29 +0300

    social login : 89.1%

---
+++ app/Library/ExtendedGoogleProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Library;
+
+use Laravel\Socialite\Two\GoogleProvider;
+
+class SocialConnect extends GoogleProvider
+{
+
+    public function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://www.googleapis.com/oauth2/v3/tokeninfo?', [
+            'query' => [
+                'id_token' => $token,
+            ],
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    protected function mapUserToObject(array $user)
+    {
+
+        return (new User)->setRaw($user)->map([
+            'id' => $user['kid'], 'nickname' => Arr::get($user, 'given_name'), 'name' => $user['name'],
+            'email' => $user['email'], 'avatar' => Arr::get($user, 'picture'),
+            'avatar_original' => preg_replace('/\?sz=([0-9]+)/', '', Arr::get($user, 'picture')),
+        ]);
+    }
+}
